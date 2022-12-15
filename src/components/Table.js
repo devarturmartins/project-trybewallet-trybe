70e2b3f8897874773382edc6e877c2d04c699941
devarturmartins <arturvl1@hotmail.com>
2022-12-15 19:29:21 -0300

    req. 7 quebrando o 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,59 +1,89 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
-        <table>
-          <tr>
-            <th>
-              Descrição
-            </th>
-          </tr>
-          <tr>
-            <th>
-              Tag
-            </th>
-          </tr>
-          <tr>
-            <th>
-              Método de pagamento
-            </th>
-          </tr>
-          <tr>
-            <th>
-              Valor
-            </th>
-          </tr>
-          <tr>
-            <th>
-              Moeda
-            </th>
-          </tr>
-          <tr>
-            <th>
-              Câmbio utilizado
-            </th>
-          </tr>
-          <tr>
-            <th>
-              Valor convertido
-            </th>
-          </tr>
-          <tr>
-            <th>
-              Moeda de conversão
-            </th>
-          </tr>
-          <tr>
-            <th>
-              Editar/Excluir
-            </th>
-          </tr>
-        </table>
+        {
+          expenses.map((e) => (
+            <table key={ e.id }>
+              <thead>
+                <tr>
+                  <th>
+                    Descrição
+                  </th>
+                  <th>
+                    Tag
+                  </th>
+                  <th>
+                    Método de pagamento
+                  </th>
+                  <th>
+                    Valor
+                  </th>
+                  <th>
+                    Moeda
+                  </th>
+                  <th>
+                    Câmbio utilizado
+                  </th>
+                  <th>
+                    Valor convertido
+                  </th>
+                  <th>
+                    Moeda de conversão
+                  </th>
+                  <th>
+                    Editar/Excluir
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    { e.description }
+                  </td>
+                  <td>
+                    { e.tag }
+                  </td>
+                  <td>
+                    { e.method }
+                  </td>
+                  <td>
+                    { Number(e.value).toFixed(2) }
+                  </td>
+                  <td>
+                    { e.exchangeRates[e.currency].name }
+                  </td>
+                  <td>
+                    { Number(e.exchangeRates[e.currency].ask).toFixed(2) }
+                  </td>
+                  <td>
+                    { (Number(e.value)
+                  * Number(e.exchangeRates[e.currency].ask)).toFixed(2) }
+                  </td>
+                  <td>
+                    Real
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          ))
+        }
       </div>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
